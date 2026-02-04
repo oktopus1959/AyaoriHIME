56d2518abfe39d60c723c87da9c24e42fe7d9ff9
OKA Toshiyuki <oka.oktopus@gmail.com>
2026-02-04 15:10:57 +0900

    FAQ

--- DyMazin/maze_tools/morph_maze.sh
+++ DyMazin/maze_tools/morph_maze.sh
@@ -2,4 +2,4 @@
 
 . ~/bin/debug_util.sh
 
-RUN_CMD -m "cat | ../bin/Release/dymaz.exe -d work/maze_ipadic/bin --userdic work/maze_ipadic/user/bin/user.dic  -L info -F'%m\t%H ||| %phl,%phr,%pb,%pw,%pC,%pn,%pc\n' --eos-format='EOS,%phl,%phr,%pb,%pw,%pC,%pn,%pc' --non-terminal-cost=5000 -N5 --maze-penalty -1 --maze-conn-penalty 3000 $*"
+RUN_CMD -m "cat | ../bin/Release/dymaz.exe -d work/maze_ipadic/bin --userdic work/maze_ipadic/user/bin/user.dic  -L info -F'%m\t%H ||| %phl,%phr,%pb,w:%pw,c:%pC,a:%pc\n' --eos-format='EOS,%phl,%phr,%pb,%pw,%pC,%pn,%pc' --non-terminal-cost=5000 -N5 --maze-penalty -1 --maze-conn-penalty 3000 $*"
