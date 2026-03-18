32d2b2b616f45315d6c4bc3a496a8762e3d66794
OKA Toshiyuki <oka.oktopus@gmail.com>
2026-03-18 09:13:12 +0900

    maze-penalty 3000

--- DyMazin/maze_tools/morph.sh
+++ DyMazin/maze_tools/morph.sh
@@ -4,4 +4,4 @@
 
 #IGNORE_EOS=--ignore-eos
 
-RUN_CMD -m "cat | ../bin/Release/dymaz.exe -d work/maze_ipadic/bin --userdic work/maze_ipadic/user/bin/user.dic  -L info -F'%m\t%H ||| %phl,%phr,%pb,%pw,%pC,%pn,%pc\n' --eos-format='EOS,%phl,%phr,%pb,%pw,%pC,%pn,%pc' --show-line-separator --non-terminal-cost=5000 -N3 --maze-penalty 1000 --maze-conn-penalty 3000 $*"
+RUN_CMD -m "cat | ../bin/Release/dymaz.exe -d work/maze_ipadic/bin --userdic work/maze_ipadic/user/bin/user.dic  -L info -F'%m\t%H ||| %phl,%phr,%pb,%pw,%pC,%pn,%pc\n' --eos-format='EOS,%phl,%phr,%pb,%pw,%pC,%pn,%pc' --show-line-separator --non-terminal-cost=5000 -N3 --maze-penalty 3000 --maze-conn-penalty 3000 $*"
